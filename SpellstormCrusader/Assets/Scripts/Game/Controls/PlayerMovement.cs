1c8911220f1d9e432f789499060b7384e402f6e0
oliver.amstutz@zuehlke.com <oliver.amstutz@zuehlke.com>
2025-05-14 11:11:56 +0200

    #5 add movement sfx

--- SpellstormCrusader/Assets/Scripts/Game/Controls/PlayerMovement.cs
+++ SpellstormCrusader/Assets/Scripts/Game/Controls/PlayerMovement.cs
@@ -4,9 +4,13 @@ namespace Game.Controls
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [SerializeField] private AudioClip[] footstepClips;
+        [SerializeField] private float footstepInterval = 0.5f;
         [SerializeField] private float moveSpeed = 5f;
         private Animator _animator;
         private PlayerControls _controls;
+        private AudioSource _footstepSource;
+        private float _footstepTimer;
         private Vector2 _moveInput;
         private Rigidbody _rb;
 
@@ -15,6 +19,7 @@ namespace Game.Controls
             _controls = new PlayerControls();
             _rb = GetComponent<Rigidbody>();
             _animator = GetComponent<Animator>();
+            _footstepSource = GetComponent<AudioSource>();
         }
 
         private void Update()
@@ -22,6 +27,7 @@ namespace Game.Controls
             _animator.SetFloat("forwardBackward", _moveInput.y);
             _animator.SetFloat("leftRight", _moveInput.x);
             // animator.SetFloat("Speed", _moveInput.sqrMagnitude);
+            HandleFootsteps();
         }
 
         private void FixedUpdate()
@@ -42,6 +48,35 @@ namespace Game.Controls
             _controls.Player.Move.performed -= ctx => _moveInput = ctx.ReadValue<Vector2>();
             _controls.Player.Move.canceled -= ctx => _moveInput = Vector2.zero;
             _controls.Player.Disable();
+        }
+
+        private void HandleFootsteps()
+        {
+            if (_moveInput.sqrMagnitude > 0.01f)
+            {
+                _footstepTimer -= Time.deltaTime;
+                if (_footstepTimer <= 0f)
+                {
+                    PlayFootstep();
+                    _footstepTimer = footstepInterval;
+                }
+            }
+            else
+            {
+                _footstepTimer = 0f;
+            }
+        }
+
+        private void PlayFootstep()
+        {
+            if (footstepClips.Length == 0)
+            {
+                return;
+            }
+
+            int index = Random.Range(0, footstepClips.Length);
+            _footstepSource.pitch = Random.Range(0.95f, 1.2f);
+            _footstepSource.PlayOneShot(footstepClips[index]);
         }
     }
 }
